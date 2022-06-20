6c474920192f016c8991a1c4bbc8d0573cc03013
onushi <4374999+onushi@users.noreply.github.com>
2022-06-20 12:41:24 +0900

    ログインフォームたたき台

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,6 +1,8 @@
 describe('top page', () => {
   it('show login form', () => {
     cy.visit('http://localhost:3000')
+
+    //FIXME:'login'という名前のフォームがあること
     cy.url().should('not.include', 'login')
   })
 })
---
+++ pages/components/LoginForm.tsx
@@ -0,0 +1,5 @@
+import { FormEvent } from "react"
+
+export default function LoginForm() {
+  return <></>
+}
